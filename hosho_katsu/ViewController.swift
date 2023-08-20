e8b774eac6cadeffb7a9ba6d62a206d40c314e21
lit-syunsuke <syunsuke_11@icloud.com>
2023-08-20 11:03:33 +0900

    init

---
+++ hosho_katsu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hosho_katsu
+//
+//  Created by わだわだ on 2023/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
